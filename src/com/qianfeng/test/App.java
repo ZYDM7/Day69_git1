7212244a18232c466a0d1c5d622ee0861f905861
ZYDM7 <ZYDM7_137368976@qq.com>
2019-04-10 21:47:13 +0800

    add info

--- src/com/qianfeng/test/App.java
+++ src/com/qianfeng/test/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("GIT1");
 
+        System.out.println("0000");
+
         ArrayList<Object> list = new ArrayList<>();
     }
 }
